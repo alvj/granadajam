5c01cdc2b6ccb64d51994218f7921afbb9c646c0
Álvaro Pérez <ibringtheweatherwme@gmail.com>
2019-01-25 20:42:49 +0100

    Player movement and basic lighting mechanic

---
+++ granada-jam/Assets/_GameAssets/_Scripts/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    [SerializeField] float speed;
+
+    Rigidbody2D rb;
+    float xAxis;
+    float yAxis;
+
+
+    void Awake()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        xAxis = Input.GetAxisRaw("Horizontal");
+        yAxis = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 velocity = rb.velocity;
+        velocity = new Vector2(xAxis, yAxis) * speed;
+        rb.velocity = velocity;
+    }
+}
