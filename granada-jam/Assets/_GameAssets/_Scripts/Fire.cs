ce85a74c02ec995e1ed3ebb30e0b6004699f6100
Ivan <sorafw1@gmail.com>
2019-01-26 20:17:48 +0100

    HAHAAHAH

---
+++ granada-jam/Assets/_GameAssets/_Scripts/Fire.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fire : MonoBehaviour {
+
+    [SerializeField] float damping;
+    
+	void Update () {
+        Vector3 pos = transform.position;
+        pos.y = Mathf.Sin(Time.time / damping);
+        this.transform.position = pos;
+	}
+}
